0f2d6e5401a629a518bde34ba5e3945cc144b09b
Donnie West <me@donniewest.com>
2021-01-28 20:21:06 -0600

    Switch to pipewire, using ponymix for volume

--- .bin/soundDown.sh
+++ .bin/soundDown.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-pulseaudio-ctl down ; pkill -RTMIN+1 i3blocks
+# pulseaudio-ctl down ; pkill -RTMIN+1 i3blocks
 # pactl set-sink-mute 0 false ; pactl set-sink-volume 0 -5% ; pkill -RTMIN+1 i3blocks
 # amixer -c 0 set PCM 5%- ; pkill -RTMIN+1 i3blocks
+ponymix decrease 5 ; pkill -RTMIN+1 i3blocks
--- .bin/soundUp.sh
+++ .bin/soundUp.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-pulseaudio-ctl up ; pkill -RTMIN+1 i3blocks
+
+# pulseaudio-ctl up ; pkill -RTMIN+1 i3blocks
 # pactl set-sink-mute 0 false ; pactl set-sink-volume 0 +5% ; pkill -RTMIN+1 i3blocks
 
 # amixer -c 0 set PCM 5%+ ; pkill -RTMIN+1 i3blocks
+ponymix increase 5 ; pkill -RTMIN+1 i3blocks

88a24a87872a1c2b6dd9b800a30b90b530857374
Donnie West <me@donniewest.com>
2022-06-29 21:28:22 -0500

    Add ability to copy qutebrowser page as markdown

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -39,7 +39,7 @@ config.load_autoconfig(False)
 # Aliases for commands. The keys of the given dictionary are the
 # aliases, while the values are the commands they map to.
 # Type: Dict
-c.aliases = {'q': 'close', 'qa': 'quit', 'w': 'session-save', 'wq': 'quit --save', 'wqa': 'quit --save', 'pass': 'spawn --userscript ~/.config/qutebrowser/userscripts/qute-bitwarden', 'mpv': 'spawn --userscript ~/.config/qutebrowser/userscripts/view_in_mpv', 'pushwebsite': 'spawn --userscript ~/.config/qutebrowser/userscripts/pushwebsite', 'readability': 'spawn --userscript ~/.config/qutebrowser/userscripts/readability-js', 'add-to-wallabag': 'spawn --userscript ~/.config/qutebrowser/userscripts/wallabag-add'}
+c.aliases = {'q': 'close', 'qa': 'quit', 'w': 'session-save', 'wq': 'quit --save', 'wqa': 'quit --save', 'pass': 'spawn --userscript ~/.config/qutebrowser/userscripts/qute-bitwarden', 'mpv': 'spawn --userscript ~/.config/qutebrowser/userscripts/view_in_mpv', 'pushwebsite': 'spawn --userscript ~/.config/qutebrowser/userscripts/pushwebsite', 'readability': 'spawn --userscript ~/.config/qutebrowser/userscripts/readability-js', 'add-to-wallabag': 'spawn --userscript ~/.config/qutebrowser/userscripts/wallabag-add', 'copy-page-to-markdown': 'spawn --userscript ~/.config/qutebrowser/userscripts/copy-as-markdown'}
 
 # Additional arguments to pass to Qt, without leading `--`. With
 # QtWebEngine, some Chromium arguments (see
